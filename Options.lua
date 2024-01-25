4a7bfe5abe64ea0504ce0ba21e5186b985fdd3ba
Serkan Yazıcıoğlu <serkanyazicioglu@garaj.io>
2024-01-25 14:15:04 +0300

    Added options

---
+++ Options.lua
@@ -0,0 +1,138 @@
+local addonName, addon = ...
+local RB = RetailBags
+
+local AceConfig = LibStub("AceConfig-3.0")
+local AceConfigDialog = LibStub("AceConfigDialog-3.0")
+
+local optionsTable = {
+    type = "group",
+    args = {
+        BagSettings = {
+            type = "group",
+            inline = true,
+            name = "Bag Settings",
+            width = "full",
+            args = {
+                DisplayBagsAutomatically = {
+                    type = "toggle",
+                    order = 1,
+                    name = "Display bags with character pane",
+                    desc = "Automatically toggles all bags when character pane visibility changes.",
+                    width = "full",
+                    get = function(info) return RB.DB.profile.displayBagsWithCharacterPane end,
+                    set = function(info, value)
+                        RB.DB.profile.displayBagsWithCharacterPane = value
+                    end,
+                },
+                DisplaySearch = {
+                    type = "toggle",
+                    order = 2,
+                    name = "Display search box",
+                    desc = "Display a search box to filter your bags.",
+                    width = "full",
+                    get = function(info) return RB.DB.profile.displaySearchBox end,
+                    set = function(info, value)
+                        RB.DB.profile.displaySearchBox = value;
+                        if (value) then
+                            RB.BagItemSearchBox:Show();
+                        else
+                            RB.BagItemSearchBox:Hide();
+                        end
+                    end,
+                },
+                DisplaySortButton = {
+                    type = "toggle",
+                    order = 3,
+                    name = "Display sort button",
+                    desc = "Display a sort button to arrange your bags.",
+                    width = "full",
+                    get = function(info) return RB.DB.profile.displaySortButton end,
+                    set = function(info, value)
+                        RB.DB.profile.displaySortButton = value;
+                        if (value) then
+                            RB.BagItemAutoSortButton:Show();
+                        else
+                            RB.BagItemAutoSortButton:Hide();
+                        end
+                    end,
+                },
+                SortBagsRightToLeft = {
+                    type = "toggle",
+                    order = 4,
+                    name = "Sort bags right to left",
+                    desc = "Changes sorting direction.",
+                    width = "full",
+                    get = function(info) return RB.DB.profile.sortBagsRightToLeft end,
+                    set = function(info, value)
+                        RB.DB.profile.sortBagsRightToLeft = value;
+                        SetSortBagsRightToLeft(value);
+                    end,
+                },
+                ContainerScale = {
+                    type = "range",
+                    order = 5,
+                    name = "Bags container scale",
+                    desc = "Change size of bag containers.",
+                    min = 1,
+                    max = 2,
+                    step = .1,
+                    get = function(info) return RB.DB.profile.bagContainerScale end,
+                    set = function(info, value)
+                        RB.DB.profile.bagContainerScale = value;
+                        CONTAINER_SCALE = RB.DB.profile.bagContainerScale;
+                    end,
+                }
+            },
+        }, TooltipSettings = {
+            type = "group",
+            inline = true,
+            name = "Tooltip Settings",
+            width = "full",
+            args = {
+                DisplayItemQuality = {
+                    type = "toggle",
+                    order = 1,
+                    name = "Display item quality",
+                    width = "full",
+                    get = function(info) return RB.DB.profile.displayTooltipItemQuality end,
+                    set = function(info, value)
+                        RB.DB.profile.displayTooltipItemQuality = value
+                    end,
+                },
+                DisplayItemLevel = {
+                    type = "toggle",
+                    order = 1,
+                    name = "Display item level",
+                    width = "full",
+                    get = function(info) return RB.DB.profile.displayTooltipItemLevel end,
+                    set = function(info, value)
+                        RB.DB.profile.displayTooltipItemLevel = value
+                    end,
+                },
+                DisplayCraftingReagent = {
+                    type = "toggle",
+                    order = 1,
+                    name = "Display crafting reagent",
+                    width = "full",
+                    get = function(info) return RB.DB.profile.displayTooltipCraftingReagent end,
+                    set = function(info, value)
+                        RB.DB.profile.displayTooltipCraftingReagent = value
+                    end,
+                },
+                DisplayVendorPrice = {
+                    type = "toggle",
+                    order = 1,
+                    name = "Display vendor price",
+                    width = "full",
+                    get = function(info) return RB.DB.profile.displayTooltipVendorPrice end,
+                    set = function(info, value)
+                        RB.DB.profile.displayTooltipVendorPrice = value
+                    end,
+                }
+            },
+        }
+    }
+  }
+
+AceConfig:RegisterOptionsTable(addonName, optionsTable, nil);
+AceConfigDialog:AddToBlizOptions(addonName);
--- RetailBags.lua
+++ RetailBags.lua
@@ -1,18 +1,49 @@
-local RetailBags = {};
-SetSortBagsRightToLeft(true);
+local addonName, addon = ...
+local RB = LibStub("AceAddon-3.0"):NewAddon(addonName);
+RetailBags = RB;
 
-hooksecurefunc("ContainerFrame_Update", function( self )
+RB.defaults = {
+	profile = {
+		version = 1,
+		displayBagsWithCharacterPane = true,
+		displaySearchBox = true,
+		displaySortButton = true,
+		sortBagsRightToLeft = true,
+		bagContainerScale = 1,
+		displayTooltipItemQuality = true,
+		displayTooltipItemLevel = true,
+		displayTooltipCraftingReagent = true,
+		displayTooltipVendorPrice = true,
+	}
+}
+
+function RB:OnInitialize()
+	self.GUI = LibStub("AceGUI-3.0")
+	self.DB = LibStub("AceDB-3.0"):New("RetailBagsDB", RB.defaults, "Default")
+	self.BagItemSearchBox = BagItemSearchBox;
+	self.BagItemAutoSortButton = BagItemAutoSortButton;
+
+	SetSortBagsRightToLeft(self.DB.profile.sortBagsRightToLeft);
+	CONTAINER_SCALE = self.DB.profile.bagContainerScale;
+end
+
+hooksecurefunc("ContainerFrame_Update", function(self)
 	if ContainerFrame_IsBackpack(self:GetID()) then
-
 		BagItemSearchBox:SetParent(self);
 		BagItemSearchBox:ClearAllPoints();
 		BagItemSearchBox:SetPoint("TOPLEFT", self, "TOPLEFT", 50, -30)
 		BagItemSearchBox.anchorBag = self;
-		BagItemSearchBox:Show();
-		
+
+		if (RB.DB.profile.displaySearchBox) then
+			BagItemSearchBox:Show();
+		end
+
 		BagItemAutoSortButton:SetParent(self);
 		BagItemAutoSortButton:SetPoint("TOPRIGHT", self, "TOPRIGHT", -9, -28);
-		BagItemAutoSortButton:Show();
+
+		if (RB.DB.profile.displaySortButton) then
+			BagItemAutoSortButton:Show();
+		end
 	elseif BagItemSearchBox.anchorBag == self then
 		BagItemSearchBox:ClearAllPoints();
 		BagItemSearchBox:Hide();
@@ -29,33 +60,37 @@ end
 local match = string.match
 local strsplit = strsplit
 
-RetailBags.Colors = {
-	yellow 		 = "|cFFFFFF00",
-	white 		 = "|cFFFFFFFF",
-	blue 		 = "|cFF69CCF0",
+RB.Colors = {
+	yellow    = "|cFFFFFF00",
+	white     = "|cFFFFFFFF",
+	blue      = "|cFF69CCF0",
 
-	common 		 = "|cFFFFFFFF",
-	uncommon 	 = "|cFF1EFF00",
-	rare 		 = "|cFF0070DD",
-	epic 		 = "|cFFA335EE",
-	legendary 	 = "|cFFFF8000",
-	artifact 	 = "|cFFE6CC80",
+	common    = "|cFFFFFFFF",
+	uncommon  = "|cFF1EFF00",
+	rare      = "|cFF0070DD",
+	epic      = "|cFFA335EE",
+	legendary = "|cFFFF8000",
+	artifact  = "|cFFE6CC80",
 };
 
 local function GameTooltip_OnTooltipSetItem(tooltip)
+	if not tooltip then return; end
+
 	local _, link = tooltip:GetItem()
 	if not link then return; end
 
 	local itemString = match(link, "item[%-?%d:]+")
 	local _, itemId = strsplit(":", itemString)
-	local itemName, _, quality, itemLevel, _, _, _, stack, slot, _, sellPrice, classId, subClassId, bindType, expacID , setID, isCraftingReagent = GetItemInfo('item:' .. itemId);
+	local itemName, _, quality, itemLevel, _, _, _, stack, slot, _, sellPrice, classId, subClassId, bindType, expacID, setID, isCraftingReagent =
+		GetItemInfo('item:' .. itemId);
 
 	local count = 1;
 	if (stack > 1) then
 		local focusedItem = GetMouseFocus();
 		if (focusedItem) then
-			count = focusedItem.count or (focusedItem.Count and focusedItem.Count:GetText()) or (focusedItem.Quantity and focusedItem.Quantity:GetText())
-			 --or (bn and _G[bn] and _G[bn]:GetText())
+			count = focusedItem.count or (focusedItem.Count and focusedItem.Count:GetText()) or
+				(focusedItem.Quantity and focusedItem.Quantity:GetText())
+			--or (bn and _G[bn] and _G[bn]:GetText())
 			count = tonumber(count) or 1
 			if count <= 1 then
 				count = 1
@@ -65,13 +100,34 @@ local function GameTooltip_OnTooltipSetItem(tooltip)
 
 	if isCraftingReagent or classId == Enum.ItemClass.Tradegoods then
 		--local r,g,b,hex = GetItemQualityColor(quality);
-		tooltip:AddLine(RetailBags.Colors.white .. _G["ITEM_QUALITY"..quality.."_DESC"]);
-		tooltip:AddLine(RetailBags.Colors.blue .. "Crafting Reagent");
-		GameTooltip_OnTooltipAddMoney(tooltip, sellPrice * count, nil);
+		if (RB.DB.profile.displayTooltipItemQuality) then
+			tooltip:AddLine(RB.Colors.white .. _G["ITEM_QUALITY" .. quality .. "_DESC"]);
+		end
+		if (RB.DB.profile.displayTooltipCraftingReagent) then
+			tooltip:AddLine(RB.Colors.blue .. "Crafting Reagent");
+		end
 	elseif itemLevel > 1 and (classId == Enum.ItemClass.Weapon or classId == Enum.ItemClass.Armor or classId == Enum.ItemClass.Projectile or classId == Enum.ItemClass.Quiver) then
-		tooltip:AddLine(RetailBags.Colors.white .. _G["ITEM_QUALITY"..quality.."_DESC"]);
-		tooltip:AddLine("Item Level " .. itemLevel);
+		if (RB.DB.profile.displayTooltipItemQuality) then
+			tooltip:AddLine(RB.Colors.white .. _G["ITEM_QUALITY" .. quality .. "_DESC"]);
+		end
+		if (RB.DB.profile.displayTooltipItemLevel) then
+			tooltip:AddLine("Item Level " .. itemLevel);
+		end
+
+		--print(itemName .. " " .. itemLevel);
+
+		if (tooltip.shoppingTooltips) then
+			--hooksecurefunc(tooltip.shoppingTooltips[1], "Show", GameTooltip_OnTooltipSetItem);
+			-- GameTooltip_OnTooltipSetItem(tooltip.shoppingTooltips[1]);
+			-- GameTooltip_OnTooltipSetItem(tooltip.shoppingTooltips[2]);
+		end
 	end
+
+	if (RB.DB.profile.displayTooltipVendorPrice and sellPrice > 0) then
+		GameTooltip_OnTooltipAddMoney(tooltip, sellPrice * count, nil);
+	end
+
+	--GameTooltip_CalculatePadding(tooltip);
 end
 
 GameTooltip:HookScript("OnTooltipSetItem", GameTooltip_OnTooltipSetItem);
@@ -85,12 +141,14 @@ hooksecurefunc(WorldMapFrame, "Show", function(self)
 end);
 
 local function CharacterFrame_VisibilityCallback(frame)
-	HideUIPanel(WorldMapFrame);
+	if (RB.DB.profile.displayBagsWithCharacterPane) then
+		HideUIPanel(WorldMapFrame);
 
-	if (frame:IsShown()) then
-		OpenAllBags();
-	else
-		CloseAllBags();
+		if (frame:IsShown()) then
+			OpenAllBags();
+		else
+			CloseAllBags();
+		end
 	end
 end
 
