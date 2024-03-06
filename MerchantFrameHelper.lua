017455d7175d10604cc87a9632bb2776fe2fab9e
Serkan Yazıcıoğlu <serkanyazicioglu@garaj.io>
2024-03-06 13:52:25 +0300

    Merchant border updates

--- MerchantFrameHelper.lua
+++ MerchantFrameHelper.lua
@@ -11,28 +11,36 @@ end
 
 function InitMerchant(func, count, pageIndex)
     for slotId = 1, count do
-        local frame = _G['MerchantItem' .. slotId .. 'ItemButton'];
+        InitMerchantSlot(func, count, pageIndex, slotId)
+    end
+end
 
-        if (RB.DB.profile.displayItemQualityBorders) then
-            local index = ((pageIndex - 1) * count) + slotId;
-            local merchantItemLink = func(index);
-            if (merchantItemLink) then
-                local itemName, _, quality, itemLevel, _, _, _, stack, slot, _, sellPrice, classId, subClassId, bindType, expacID, setID, isCraftingReagent =
-                    GetItemInfo(merchantItemLink);
+function InitMerchantSlot(func, count, pageIndex, slotId)
+    local frame = _G['MerchantItem' .. slotId .. 'ItemButton'];
 
-                if quality then
-                    local color = ITEM_QUALITY_COLORS[quality];
-                    _G["MerchantItem" .. slotId].Name:SetTextColor(color.r, color.g, color.b);
+    if (not frame) then
+        return;
+    end
 
-                    CreateBorder(frame, quality, classId, true);
-                    return;
-                end
+    if (RB.DB.profile.displayItemQualityBorders) then
+        local index = ((pageIndex - 1) * count) + slotId;
+        local merchantItemLink = func(index);
+        if (merchantItemLink) then
+            local itemName, _, quality, itemLevel, _, _, _, stack, slot, _, sellPrice, classId, subClassId, bindType, expacID, setID, isCraftingReagent =
+                GetItemInfo(merchantItemLink);
+
+            if quality then
+                local color = ITEM_QUALITY_COLORS[quality];
+                _G["MerchantItem" .. slotId].Name:SetTextColor(color.r, color.g, color.b);
+
+                CreateBorder(frame, quality, classId, true);
+                return;
             end
         end
+    end
 
-        if (frame.RetailBagsBorder) then
-            frame.RetailBagsBorder:Hide();
-        end
+    if (frame.RetailBagsBorder) then
+        frame.RetailBagsBorder:Hide();
     end
 end
 
